104f78d0b2551057129cceeb47e99674177e649c
JOVANY ALBEIRO LOPEZ ZAPATA <jovany_lopez82101@elpoli.edu.co>
2022-05-02 13:35:47 -0500

    HU002 Jovany

---
+++ classes/Persona.py
@@ -0,0 +1,9 @@
+class Persona:
+    def __init__(self):
+        self.nombre=None
+        self.edad=None
+        self.telefono=None
+
+def saludar(self):
+    print(f'Hola me llamo {self.nombre}')
+    print(f'mi edad es {self.edad} y me gusta programar')
