f4a9990578e9c3b84c8fa0ce9d10510f08cd9998
401 <401@academicos.cesde.edu.co>
2022-05-02 13:22:38 -0500

    HU002-Valentina

---
+++ classes/Persona.py
@@ -0,0 +1,11 @@
+class Persona:
+
+    def _init_(self):
+
+        self.nombre=None
+        self.edad=None
+        self.telefono=None
+
+    def saludar(self):
+        print(f'Hola me llamo {self.nombre}')    
+        print(f'Mi edad es {self.edad}')  
