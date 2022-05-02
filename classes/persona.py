6797eecfe237112c9a803db78eb3736e3bf73fbc
401 <401@academicos.cesde.edu.co>
2022-05-02 13:21:39 -0500

    HUOO7 Sebastian Bedoya

---
+++ classes/persona.py
@@ -0,0 +1,13 @@
+from cgi import print_arguments
+
+
+class Persona:
+
+    def__init__(self):
+        self.nombre=None
+        self.edad=None
+        self.telefono=None
+    def saludarr(self);
+        print(f'hola me llamo {self.nombre}')
+        print("jajajaj")
+
