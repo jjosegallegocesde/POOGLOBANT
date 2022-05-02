04a42a5bfa932a20fdd415ffdfedfe8c50340818
401 <401@academicos.cesde.edu.co>
2022-05-02 13:21:38 -0500

    HU002 - Karen Rodriguez

---
+++ classes/Persona.py
@@ -0,0 +1,12 @@
+class Persona:
+    
+    def __init__(self):
+        self.nombre= None
+        self.edad=None
+        self.telefono=None
+    
+    def saludar(self):
+        print(f'Hola hermos@ {self.nombre}')
+        print(f'<3')
+        
+
