a99804447f0c51bae81d902adea3301a9bbf212b
401 <401@academicos.cesde.edu.co>
2022-05-02 13:23:19 -0500

    HU002-JHON

---
+++ classes/Persona.py
@@ -0,0 +1,11 @@
+class Persona:
+
+    def __init__(self):                                  #Constructor de la clase
+        self.nombre=None
+        self.edad=None
+        self.telefono=None
+
+    def saludar(self):
+        print(f'Hola me llamo {self.nombre}')
+        print('Lo que quiero es vivir en europa 7w7')
+
