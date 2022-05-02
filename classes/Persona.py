0d61957c0230af5c2bd1e4d005c0a6d30534b8d8
saimon <simonclagutierrez@gmailcom>
2022-05-02 13:33:23 -0500

    HU02-Simon Clavijo Gutierrez

---
+++ classes/Persona.py
@@ -0,0 +1,11 @@
+class Persona : 
+    def __init__(self):
+        self.nombre=None
+        self.edad=None
+        self.telefono=None
+
+    def saludar(self):
+        print(f'Hola me llamo {self.nombre}')
+        print('Me gustan las Salchipapas')
+    
+        
