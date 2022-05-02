097bb77a1cf9eb7390c4fc152c835d12aeaad376
401 <401@academicos.cesde.edu.co>
2022-05-02 13:32:35 -0500

    se crea la clase persona

--- classes/persona.py
+++ classes/persona.py
@@ -3,11 +3,12 @@ from cgi import print_arguments
 
 class Persona:
 
-    def__init__(self):
+    def __init__(self,edad,nombre,telefono):
         self.nombre=None
         self.edad=None
         self.telefono=None
-    def saludarr(self);
+        
+    def saludarr(self):
         print(f'hola me llamo {self.nombre}')
         print("jajajaj")
 
